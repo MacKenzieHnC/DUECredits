fe6ef7bea9a652dd1907a3a5cbe37cfa692033a3
mackenzie <mackenzie@yourmoms>
2022-04-20 22:01:57 -0500

    Initialize React Native

---
+++ DUECredits/windows/DUECredits/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::DUECredits::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::DUECredits::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
