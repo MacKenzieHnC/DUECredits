7dd72afafbd0ced5f372a2b2bbeab2acf38d1993
mackenzie <mackenzie@yourmoms>
2022-05-03 08:36:38 -0500

    Fix spacing issue on ItemComponent

--- components/Item.tsx
+++ components/Item.tsx
@@ -1,5 +1,6 @@
-import {Box, HStack, VStack, Text} from 'native-base';
+import {Box, HStack, VStack, Text, Spacer} from 'native-base';
 import React from 'react';
+import {StyleSheet} from 'react-native-windows';
 import {ArmorItem, WeaponItem} from '../models/ItemIndex';
 
 interface ItemProps {
@@ -11,22 +12,35 @@ export const ItemComponent = ({item, children}: ItemProps) => {
   const isRestricted = item.itemProps.restricted;
 
   return (
-    <Box width="100%" p={5} rounded="md" backgroundColor="primary.900" mb={2}>
-      <VStack space={2}>
-        <HStack justifyContent="space-between">
-          <Text color="white">
+    <Box maxWidth="100%" p={5} rounded="md" backgroundColor="primary.900" m={2}>
+      <HStack alignItems="center">
+        <VStack space={2} style={styles.nameContainer}>
+          <Text color="white" style={styles.nameText}>
             {isRestricted ? '(R) ' : ''}
             {item.itemProps.name}
           </Text>
+          {children}
+        </VStack>
+        <Box style={styles.priceContainer}>
           <Text color="white">Price: {item.itemProps.price}</Text>
-        </HStack>
-        {children}
-        {!!item.itemProps.notes && (
-          <Box>
-            <Text color="white">Notes: {item.itemProps.notes}</Text>
-          </Box>
-        )}
-      </VStack>
+        </Box>
+      </HStack>
+      {!!item.itemProps.notes && (
+        <Box>
+          <Text color="white">Notes: {item.itemProps.notes}</Text>
+        </Box>
+      )}
     </Box>
   );
 };
+
+const styles = StyleSheet.create({
+  priceContainer: {
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+    width: 150,
+    backgroundColor: 'red',
+  },
+  nameContainer: {flex: 1},
+  nameText: {flexWrap: 'wrap'},
+});
