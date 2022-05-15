a610ee9c3f8e8acc108fb1f11677bc39db565092
mackenzie <mackenzie@yourmoms>
2022-05-15 18:50:06 -0500

    Implement other special features

--- components/ListComponents/StarshipItem.tsx
+++ components/ListComponents/StarshipItem.tsx
@@ -1,11 +1,16 @@
-import {HStack, Text} from 'native-base';
+import {HStack, Text, VStack} from 'native-base';
 import React, {memo} from 'react';
+import {useGetDBStateQuery} from '../../store/slices/databaseSlice';
 import {useTheme} from '../Theme';
 import {ItemComponent} from './Item';
 
 export const StarshipItemComponent = memo(({item}) => {
+  const {data: dbState, isLoading} = useGetDBStateQuery();
   // Stylize
   const theme = useTheme();
+  if (isLoading || !dbState) {
+    return <></>;
+  }
 
   return (
     <ItemComponent item={item}>
@@ -13,6 +18,21 @@ export const StarshipItemComponent = memo(({item}) => {
         <Text color={theme.text}>HP: {item.hardpoints}</Text>
         <Text color={theme.text}>Encum: {item.encumbrance}</Text>
       </HStack>
+
+      {item.additional_rules && (
+        <VStack flex={1}>
+          <Text color={theme.text}>
+            {'Additional Rules: ' +
+              item.additional_rules
+                .map(
+                  rule =>
+                    dbState.additional_rules.find(x => x.id === rule.id).name + // Won't be undefined or should fail
+                    (rule.modifier !== '' ? ': ' + rule.modifier : ''),
+                )
+                .join(', ')}
+          </Text>
+        </VStack>
+      )}
     </ItemComponent>
   );
 });
--- components/ListComponents/VehicleWeaponItem.tsx
+++ components/ListComponents/VehicleWeaponItem.tsx
@@ -1,17 +1,37 @@
-import {HStack, Text} from 'native-base';
+import {HStack, Text, VStack} from 'native-base';
 import React, {memo} from 'react';
+import {useGetDBStateQuery} from '../../store/slices/databaseSlice';
 import {useTheme} from '../Theme';
 import {ItemComponent} from './Item';
 
 export const VehicleWeaponItemComponent = memo(({item}) => {
+  const {data: dbState, isLoading} = useGetDBStateQuery();
   // Stylize
   const theme = useTheme();
+  if (isLoading || !dbState) {
+    return <></>;
+  }
 
   return (
     <ItemComponent item={item}>
       <HStack space={3}>
         <Text color={theme.text}>Damage: {item.damage}</Text>
       </HStack>
+
+      {item.weapon_effects && (
+        <VStack flex={1}>
+          <Text color={theme.text}>
+            {'Effects: ' +
+              item.weapon_effects
+                .map(
+                  effect =>
+                    dbState.weapon_effects.find(x => x.id === effect.id).name + // Won't be undefined or should fail
+                    (effect.modifier !== '' ? ': ' + effect.modifier : ''),
+                )
+                .join(', ')}
+          </Text>
+        </VStack>
+      )}
     </ItemComponent>
   );
 });
